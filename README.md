ba77463b34e41542e205804d6a4e44826035728d
AtilaDev <atiladevelop@gmail.com>
2018-11-20 21:38:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Basic Redux example
+
+```
+npm install
+```
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const RootApp = () => (
+  <Provider
+    store={createStore(
+      reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(<RootApp />, document.querySelector('#root'));
